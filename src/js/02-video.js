3fc114850d394350ebafede7c0252a346c2b049a
AndreyDol <113788568+AndreyDol@users.noreply.github.com>
2023-02-25 16:40:21 +0200

    add 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,20 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+KEYSTORAGE = 'videoplayer-current-time';
 const iframe = document.querySelector('#vimeo-player');
 //console.log(iframe);
 const player = new Player(iframe);
+//Переводит время видео на время из localstorage
+player.setCurrentTime(localStorage.getItem(KEYSTORAGE) || 0);
 
-player.on('timeupdate', function (event) {
-  console.log(event.seconds);
-});
+//Записивает в localstorage время с видео раз в секунду
+player.on(
+  'timeupdate',
+  throttle(function (event) {
+   // console.log(event.seconds);
+    localStorage.setItem(KEYSTORAGE, event.seconds);
+  }, 1000)
+);
+
+//console.log(localStorage.getItem(KEYSTORAGE));
